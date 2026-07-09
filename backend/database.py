4ea7da3eddabcb448f53272b36f7cdd98888251b
sai-kumar20 <baratamsaikumar1427>
2026-07-09 15:39:54 +0530

    Use Railway environment variable for database

--- backend/database.py
+++ backend/database.py
@@ -1,23 +1,21 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# Railway MySQL Connection
-DATABASE_URL = "mysql+pymysql://root:CLcvtOfXqTPXPrQjegAYdXRVddOuTrCE@hayabusa.proxy.rlwy.net:13152/railway"
+# Railway / Local Database URL
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Engine
 engine = create_engine(
     DATABASE_URL,
     echo=False
 )
 
-# Session
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
 
-# Dependency
 def get_db():
     db = SessionLocal()
     try:
